9333917994909c4ea1581d8e0a994df71dfe711c
yjoonjang <yjoonjang34@gmail.com>
2023-08-04 02:44:56 +0900

    feat: add DeleteView

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = "accountapp"
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create'), #class 형에서는 as_view()를 사용해야 함
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
-    path('update/<int:pk>', AccountUpdateView.as_view(), name='update')
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delte/<int:pk>', AccountDeleteView.as_view(), name='delete')
 ]
