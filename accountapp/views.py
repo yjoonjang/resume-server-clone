3ab5119b51642e4c1e513ad621ea78705093fe06
yjoonjang <yjoonjang34@gmail.com>
2023-08-05 20:27:29 +0900

    feat: add ownership with decorator

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,19 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
+has_ownership = [account_ownership_required, login_required]
 
 # Create your views here.
 
+@login_required
 def hello_world(request):
 
-    if request.method == "POST":
+    if request.user.is_authenticated:
 
         temp = request.POST.get('hello_world_input')
 
@@ -28,6 +33,7 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
+
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
@@ -39,6 +45,8 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -46,6 +54,8 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')  # reverse_lazy는 Class형에서 사용한다.
     template_name = 'accountapp/update.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
