061ddcc482d0ceba66b4947b3f007b7a1157f3cb
yjoonjang <yjoonjang34@gmail.com>
2023-09-12 03:20:54 +0900

    feat: make projectapp

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DetailView
 
+from projectapp.decorators import account_ownership_required
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
@@ -13,16 +15,12 @@ class ProjectListView(ListView):
     template_name = 'projectapp/list.html'
     paginate_by = 5
 
+@method_decorator(account_ownership_required, 'get')
+@method_decorator(account_ownership_required, 'post')
 class ProjectCreateView(CreateView):
     model = Project
     form_class = ProjectCreationForm
     template_name = 'projectapp/create.html'
-
-    def form_valid(self, form):
-        temp_article = form.save(commit=False)
-        temp_article.writer = self.request.user
-        temp_article.save()
-        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
