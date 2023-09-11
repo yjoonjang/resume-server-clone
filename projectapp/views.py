5946bb144021bd2654c38be7ec1025ef1d36474f
yjoonjang <yjoonjang34@gmail.com>
2023-09-12 03:36:12 +0900

    feat: fix models and make articles in projectapp

--- projectapp/views.py
+++ projectapp/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic.list import MultipleObjectMixin
 
+from articleapp.models import Article
 from projectapp.decorators import account_ownership_required
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
@@ -26,10 +28,16 @@ class ProjectCreateView(CreateView):
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(project=self.get_object())
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
     def create_article(request, pk):
         ...
